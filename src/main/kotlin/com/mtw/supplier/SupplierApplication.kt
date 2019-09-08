93393c2d9d1d8223290fc1d7f55c275d39c1912a
MoyTW <moytravis@gmail.com>
2019-09-08 02:20:32 -0700

    Initial commit

---
+++ src/main/kotlin/com/mtw/supplier/SupplierApplication.kt
@@ -0,0 +1,11 @@
+package com.mtw.supplier
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SupplierApplication
+
+fun main(args: Array<String>) {
+	runApplication<SupplierApplication>(*args)
+}
