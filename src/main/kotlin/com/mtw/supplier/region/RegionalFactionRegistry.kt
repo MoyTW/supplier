e606756c1ca9df1e21a4d232595572ad96d6c294
Travis Moy <tmoy@sofi.org>
2019-10-06 09:54:48 -0700

    Tests for RegionalFactionRegistry

--- src/main/kotlin/com/mtw/supplier/region/RegionalFactionRegistry.kt
+++ src/main/kotlin/com/mtw/supplier/region/RegionalFactionRegistry.kt
@@ -1,5 +1,8 @@
 package com.mtw.supplier.region
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 class RegionalFactionRegistry {
     private val factions: MutableMap<Int, RegionalFaction> = mutableMapOf()
 
@@ -9,15 +12,21 @@ class RegionalFactionRegistry {
                 "Could not add faction [${regionalFaction.id},${regionalFaction.name}] as it already exists!")
         }
 
+        this.factions[regionalFaction.id] = regionalFaction
+
         // Ensure full coverage in relationship matrix
         for ((_,faction) in factions) {
             faction.getRelationshipScore(regionalFaction.id)
             regionalFaction.getRelationshipScore(faction.id)
         }
-        this.factions[regionalFaction.id] = regionalFaction
+
         return this
     }
     class FactionAlreadyExistsException(message: String): Exception(message)
+
+    fun getFactionName(factionId: Int): String {
+        return factions[factionId]?.name ?: throw FactionIdNotFoundException(factionId)
+    }
 
     fun getRelationshipScore(leftFactionId: Int, rightFactionId: Int) : Double {
         val leftFaction = factions[leftFactionId] ?: throw FactionIdNotFoundException(leftFactionId)
---
+++ src/test/kotlin/com/mtw/supplier/region/RegionalFactionRegistryTests.kt
@@ -0,0 +1,61 @@
+package com.mtw.supplier.region
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class EntityTests {
+    @Test
+    fun testSerializationToJson() {
+        val json = Json(JsonConfiguration.Stable)
+
+        val beastFaction = RegionalFaction(0, "beasts", mutableMapOf(0 to 100.0, 1 to -10.0))
+        val expeditionFaction = RegionalFaction(1, "expedition", mutableMapOf(0 to -25.0, 1 to 10.0))
+        val registry = RegionalFactionRegistry()
+            .addFaction(beastFaction)
+            .addFaction(expeditionFaction)
+
+        val jsonData = json.stringify(RegionalFactionRegistry.serializer(), registry)
+        Assert.assertEquals(
+            "{\"factions\":{\"0\":{\"id\":0,\"name\":\"beasts\",\"factionIdsToRelationshipScores\":{\"0\":100.0,\"1\":-10.0}},\"1\":{\"id\":1,\"name\":\"expedition\",\"factionIdsToRelationshipScores\":{\"0\":-25.0,\"1\":10.0}}}}",
+            jsonData)
+    }
+
+    @Test
+    fun testSerializationFromJson() {
+        val jsonString = "{\"factions\":{\"0\":{\"id\":0,\"name\":\"beasts\",\"factionIdsToRelationshipScores\":{\"0\":100.0,\"1\":-10.0}},\"1\":{\"id\":1,\"name\":\"expedition\",\"factionIdsToRelationshipScores\":{\"0\":-25.0,\"1\":10.0}}}}"
+        val json = Json(JsonConfiguration.Stable)
+        val registry = json.parse(RegionalFactionRegistry.serializer(), jsonString)
+
+        Assert.assertEquals("beasts", registry.getFactionName(0))
+        Assert.assertEquals("expedition", registry.getFactionName(1))
+        Assert.assertEquals(100.0, registry.getRelationshipScore(0, 0), 0.0)
+        Assert.assertEquals(-10.0, registry.getRelationshipScore(0, 1), 0.0)
+        Assert.assertEquals(-25.0, registry.getRelationshipScore(1, 0), 0.0)
+        Assert.assertEquals(10.0, registry.getRelationshipScore(1, 1), 0.0)
+    }
+
+    @Test(expected = RegionalFaction.UndefinedRelationshipException::class)
+    fun testThrowsExceptionWhenMissingRating() {
+        val beastFaction = RegionalFaction(0, "beasts", mutableMapOf(0 to 0.0))
+        val expeditionFaction = RegionalFaction(1, "expedition", mutableMapOf(0 to 0.0, 1 to 10.0))
+        RegionalFactionRegistry()
+            .addFaction(beastFaction)
+            .addFaction(expeditionFaction)
+    }
+
+    @Test(expected = RegionalFactionRegistry.FactionAlreadyExistsException::class)
+    fun testThrowsWhenDoubleAdding() {
+        val beastFaction = RegionalFaction(0, "beasts", mutableMapOf(0 to 0.0, 1 to 0.0))
+        val expeditionFaction = RegionalFaction(0, "expedition", mutableMapOf(0 to 5.0, 1 to 0.0))
+        RegionalFactionRegistry()
+            .addFaction(beastFaction)
+            .addFaction(expeditionFaction)
+    }
+}
